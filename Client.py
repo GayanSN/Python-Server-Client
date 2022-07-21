bd863ce36d69c5e0b12ea2576ee99c5cf99e8fb4
Gayan Nawarathna <79858264+GayanSN@users.noreply.github.com>
2022-07-22 02:53:41 +0530

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,38 +1,55 @@
 #! /usr/bin/python3
 
-import socket
+import socket 
+from socket import error
 import sys
+import os
 from threading import Thread
 
 def client():
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    client.connect((host, port))
+    try:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        client.connect((host, port))
 
-    while True:
-        try:
-            ts = Thread(target=send_data, args=(client,), daemon = True)
-            tr = Thread(target=recv_data, args=(client,), daemon = True)
+        while True:
+            try:
+                ts = Thread(target=send_data, args=(client,), daemon = True)
+                tr = Thread(target=recv_data, args=(client,), daemon = True)
 
-            ts.start()
-            tr.start()
-                
-            ts.join()
-            tr.join()
+                ts.start()
+                tr.start()
 
-        except KeyboardInterrupt:
-            client.close()
-            exit(1)
+                ts.join()
+                tr.join()
+
+            except KeyboardInterrupt:
+                client.close()
+                os._exit(1)
+
+    
+    except ConnectionRefusedError:
+            print(f"\nCan't Connect with the Server!!!\nPlease check whether Server application is running and try again.\n")
+
 
 def send_data(client,):
     while True:
         s_data = input("")
+
+        if len(s_data.strip()) == 0:
+            s_data = ' '
+
         client.send(s_data.encode('utf-8'))
+
 
 def recv_data(client,):
     while True:
         r_data = client.recv(1024).decode('utf-8')
         print(r_data)
+        
+        if r_data == "":
+            client.close()
+            os._exit(1)
 
 
 if __name__ == "__main__":
@@ -46,3 +63,5 @@ if __name__ == "__main__":
     host = "127.0.0.1"
 
     client()
+    
+    
