4fa43809a72dcfac35a2515c651ac9f0f8896efd
Gayan Nawarathna <79858264+GayanSN@users.noreply.github.com>
2022-07-22 02:56:21 +0530

    Update Server.py

--- Server.py
+++ Server.py
@@ -2,41 +2,57 @@
 
 import socket
 import sys
+import os
 from threading import Thread
 
 def server():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    try:
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    server.bind((host, port))
-    server.listen()
-    conn, addr = server.accept()
+        server.bind((host, port))
+        server.listen()
+        conn, addr = server.accept()
 
-    while True:
-        try:
-            ts = Thread(target=send_data, args=(conn,), daemon = True)
-            tr = Thread(target=recv_data, args=(conn,), daemon = True)
+        while True:
+            try:
+                ts = Thread(target=send_data, args=(conn,), daemon = True)
+                tr = Thread(target=recv_data, args=(conn,), daemon = True)
 
-            ts.start()
-            tr.start()
+                ts.start()
+                tr.start()
 
-            ts.join()
-            tr.join()
-            
-        except KeyboardInterrupt:
-            conn.close()
-            exit(1)
+                ts.join()
+                tr.join()
+
+            except KeyboardInterrupt:
+                conn.close()
+                os._exit(1)
+
+
+    except KeyboardInterrupt:
+        server.close()
+        os._exit(1)
 
 
 def send_data(conn,):
-        while True:
-            s_data = input("")
-            conn.send(s_data.encode('utf-8'))
+    while True:
+        s_data = input("")
+
+        if len(s_data.strip()) == 0:
+            s_data = ' '
+
+        conn.send(s_data.encode('utf-8'))
+
 
 def recv_data(conn,):
-        while True:
-            r_data = conn.recv(1024).decode('utf-8')
-            print(r_data)
+    while True:
+        r_data = conn.recv(1024).decode('utf-8')
+        print(r_data)
+
+        if r_data == "":
+            conn.close()
+            os._exit(1)
 
 
 if __name__ == "__main__":
@@ -50,3 +66,6 @@ if __name__ == "__main__":
     host = "127.0.0.1"
 
     server()
+
+    
+    
